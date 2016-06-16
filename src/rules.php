402a48061952321bc8190a1114150d9ba94766ff
Shankar Thiyagaraajan <shankarbala33@users.noreply.github.com>
2016-06-16 20:48:33 +0530

    Initiate Rules for the Form

---
+++ src/rules.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace hankarbala33/php-validation;
+
+
+/**
+ * Class Rules for Manage Rules for Different Forms
+ * @package StorePress\Helper
+ */
+class Rules
+{
+    public static function customer_address()
+    {
+        return [
+            'fname' => 'Required',
+            'lname' => 'Required',
+            'mobile' => 'Required',
+            'address1' => 'Required',
+            'address2' => 'Required',
+            'city' => 'Required',
+            'zipcode' => 'Required',
+            'country' => 'Required'
+        ];
+    }
+
+}
